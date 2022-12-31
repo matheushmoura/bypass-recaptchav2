7d36f469978583a44d9af3471ebcee0b8123fc2f
matheus <matheusshutt2@gmail.com>
2022-12-31 14:51:15 -0300

    add to try more than once time

--- bypass.py
+++ bypass.py
@@ -6,32 +6,46 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
+def gravar_audio_captcha(arquivo_audio, sample_rate, segundos):
+    with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=sample_rate) as mic:
+        data = mic.record(numframes=sample_rate * segundos)
+        sf.write(file=arquivo_audio, data=data[:, 0], samplerate=sample_rate)
+
+def audio_para_texto_captcha(arquivo_audio, segundos):
+    r = sr.Recognizer()
+    with sr.AudioFile(arquivo_audio) as source:
+        audio = r.record(source, duration=segundos)
+        text = r.recognize_google(audio, language='en-US', show_all=True)
+        return text
 
 def bypasscaptchaV2(driver, submit, iframe):
     arquivo_audio = "recaptcha_"+str(datetime.now().strftime("%Y-%m-%d_%H-%M"))+".wav"
     sample_rate = 48000
     segundos = 5
+    nao_resolveu_captcha = True
 
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, submit))).click()
     driver.switch_to.frame( WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.CSS_SELECTOR, iframe))))
     WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.ID, "recaptcha-audio-button"))).click()
-    WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//button[contains(.,'REPRODUZIR')]"))).click()
 
-    with sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True).recorder(samplerate=sample_rate) as mic:
-        data = mic.record(numframes=sample_rate * segundos)
-        sf.write(file=arquivo_audio, data=data[:, 0], samplerate=sample_rate)
-
-    r = sr.Recognizer()
-    with sr.AudioFile(arquivo_audio) as source:
-        audio = r.record(source, duration=segundos)
-        text = r.recognize_google(audio, language='en-US', show_all=True)
+    while nao_resolveu_captcha:
+        WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//button[contains(.,'REPRODUZIR')]"))).click()
+        gravar_audio_captcha(arquivo_audio, sample_rate, segundos)
+        text = audio_para_texto_captcha(arquivo_audio, segundos)
         try:
             print(' > Resposta: ', text['alternative'][0]['transcript'])
             driver.find_element(By.ID, "audio-response").send_keys(text['alternative'][0]['transcript'])
             driver.find_element(By.XPATH, "//button[contains(.,'Verificar')]").click()
-            return True
-        except: return False
-
+            try:
+                # Mensagem de erro
+                print(WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.XPATH, "//div[@class='rc-audiochallenge-error-message']"))).text)
+                driver.find_element(By.XPATH, "//button[@id='recaptcha-reload-button']").click()
+            except:
+                # Resolveu
+                return True
+        except:
+            # Não conseguiu resolver
+            driver.find_element(By.XPATH, "//button[@id='recaptcha-reload-button']").click()
 
 
 
